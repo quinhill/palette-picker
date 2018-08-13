dfab6792b2a1b0cf2048859babf5eae204551d61
Quin Hill <quinhill@icloud.com>
2018-08-13 15:59:28 -0600

    Generate random colors function

--- scripts.js
+++ scripts.js
@@ -6,5 +6,30 @@ function randomColor() {
 }
 
 function generatePalette() {
-  $('#color-one').css("backgroundColor") = "red"
+  getColor1(randomColor())
+  getColor2(randomColor())
+  getColor3(randomColor())
+  getColor4(randomColor())
+  getColor5(randomColor())
+}
+
+function getColor1(color) {
+  $('#color-one').css("backgroundColor", color)
+  
+}
+
+function getColor2(color) {
+  $('#color-two').css("backgroundColor", color)
+}
+
+function getColor3(color) {
+  $('#color-three').css("backgroundColor", color)
+}
+
+function getColor4(color) {
+  $('#color-four').css("backgroundColor", color)
+}
+
+function getColor5(color) {
+  $('#color-five').css("backgroundColor", color)
 }
