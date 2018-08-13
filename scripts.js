8202734fe4a1c59de311a0e448e2c41836dbba07
Quin Hill <quinhill@icloud.com>
2018-08-13 16:25:07 -0600

    Add hex codes to color divs

--- scripts.js
+++ scripts.js
@@ -15,21 +15,25 @@ function generatePalette() {
 
 function getColor1(color) {
   $('#color-one').css("backgroundColor", color)
-  
+  $('#color-name-one').text(`${color}`)
 }
 
 function getColor2(color) {
   $('#color-two').css("backgroundColor", color)
+  $('#color-name-two').text(`${color}`)
 }
 
 function getColor3(color) {
   $('#color-three').css("backgroundColor", color)
+  $('#color-name-three').text(`${color}`)
 }
 
 function getColor4(color) {
   $('#color-four').css("backgroundColor", color)
+  $('#color-name-four').text(`${color}`)
 }
 
 function getColor5(color) {
   $('#color-five').css("backgroundColor", color)
+  $('#color-name-five').text(`${color}`)
 }
